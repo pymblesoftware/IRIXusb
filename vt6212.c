c3b865ef34df5c4383fe5f8c80e19c799bfb0b56
Regan Russell <regan@pymblesoftware.com>
2011-09-26 02:49:44 -0700

    Removed notice.

--- vt6212.c
+++ vt6212.c
@@ -1,15 +1,6 @@
 /**********************************************************************
-*              Copyright (C) 1990-1993, Silicon Graphics, Inc.        *
-* These coded instructions, statements, and computer programs contain *
-* unpublished proprietary information of Silicon Graphics, Inc., and *
-* are protected by Federal copyright law. They may not be disclosed *
-* to third parties or copied or duplicated in any form, in whole or   *
-* in part, without prior written consent of Silicon Graphics, Inc.    *
+ *  PymbleSoftware Pty Ltd, BSDero and nekoware contriubtors. 
 **********************************************************************/
-#ident "io/sample_pciio.c: $Revision: 1.16 $"
-
-
-
 
 #include <sys/types.h>
 #include <sys/cpu.h>
